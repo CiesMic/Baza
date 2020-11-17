2c344250e64d071e29d5cc6ac62cdf5345295ce2
Michał Cieśla <zwariowanymichal@outlook.com>
2020-11-17 16:38:38 +0100

    Added 3 variables, constructor and void for writing in MainWindow

--- MainCheck/MainCheck/Baza.cs
+++ MainCheck/MainCheck/Baza.cs
@@ -10,6 +10,30 @@ namespace MainCheck
 {
     public class Baza
     {
-
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long PESEL { get; set; }
+        public Baza()
+        {
+            Name = " ";
+            Surname = " ";
+            PESEL = 0;
+        }
+        public Baza(string Name, string Surname, long PESEL)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.PESEL = PESEL;
+        }
+        public Baza(Baza o)
+        {
+            Name = o.Name;
+            Surname = o.Surname;
+            PESEL = o.PESEL;
+        }
+        public void Write(ListBox lb)
+        {
+            lb.Items.Add(Name + " " + Surname + " " + PESEL);
+        }
     }
 }
